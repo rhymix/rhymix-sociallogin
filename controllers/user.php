8f5d86bafc88af260382d93cfba204bca2899534
Kijin Sung <kijin@kijinsung.com>
2022-10-17 19:32:52 +0900

    view, mobile 클래스를 Controllers\User로 통합

--- controllers/user.php
+++ controllers/user.php
@@ -1,33 +1,49 @@
 <?php
 
-class SocialloginView extends \Rhymix\Modules\Sociallogin\Base
+namespace Rhymix\Modules\Sociallogin\Controllers;
+
+use Context;
+use MemberController;
+use MemberModel;
+use Mobile;
+use SocialloginModel;
+use Rhymix\Framework\Exceptions\InvalidRequest;
+use Rhymix\Modules\Sociallogin\Base;
+
+class User extends Base
 {
 	/**
 	 * @brief Initialization
 	 */
-	function init()
+	public function init()
 	{
-		Context::set('config', self::getConfig());
+		$config = $this->getConfig();
+		if (Mobile::isFromMobilePhone() && !empty($config->mskin))
+		{
+			$this->setTemplatePath(sprintf('%sm.skins/%s/', $this->module_path, $config->mskin));
+		}
+		else
+		{
+			$this->setTemplatePath(sprintf('%sskins/%s/', $this->module_path, $config->skin ?? 'default'));
+		}
 
-		$this->setTemplatePath(sprintf('%sskins/%s/', $this->module_path, self::getConfig()->skin));
-
-		//HACK: 현재는 AddJsFile을 유지시킨다. 추후 loadFile이나 해당 메서드가 변경되면 그때 수정.
-		Context::addJsFile($this->module_path . 'tpl/js/sociallogin.js');
+		Context::set('config', $config);
+		Context::loadFile([$this->module_path . 'tpl/js/sociallogin.js', 'body']);
 	}
 
 	/**
 	 * @brief SNS 관리
 	 */
-	function dispSocialloginSnsManage()
+	public function dispSocialloginSnsManage()
 	{
 		if (!Context::get('is_logged'))
 		{
-			throw new Rhymix\Framework\Exception('msg_not_logged');
+			throw new \Rhymix\Framework\Exception('msg_not_logged');
 		}
 
 		foreach (self::getConfig()->sns_services as $key => $val)
 		{
-			$args = new stdClass;
+			$args = new \stdClass;
 			$sns_info = SocialloginModel::getMemberSnsByService($val);
 			
 			if ($sns_info->name)
@@ -54,49 +70,48 @@ class SocialloginView extends \Rhymix\Modules\Sociallogin\Base
 	/**
 	 * @brief SNS 연결 진행
 	 */
-	function dispSocialloginConnectSns()
+	public function dispSocialloginConnectSns()
 	{
 		if (isCrawler())
 		{
-			throw new Rhymix\Framework\Exceptions\InvalidRequest();
+			throw new InvalidRequest;
 		}
 		
 		$service = Context::get('service');
 		if (!$service || !in_array($service, self::getConfig()->sns_services))
 		{
-			throw new Rhymix\Framework\Exception('msg_not_support_service_login');
+			throw new \Rhymix\Framework\Exception('msg_not_support_service_login');
 		}
 		if (!$oDriver = $this->getDriver($service))
 		{
-			throw new Rhymix\Framework\Exceptions\InvalidRequest();
+			throw new InvalidRequest;
 		}
 		
 		if (!$type = Context::get('type'))
 		{
-			throw new Rhymix\Framework\Exceptions\InvalidRequest();
+			throw new InvalidRequest;
 		}
 
 		if ($type == 'register' && !Context::get('is_logged'))
 		{
-			throw new Rhymix\Framework\Exception('msg_not_logged');
+			throw new \Rhymix\Framework\Exception('msg_not_logged');
 		}
 		else if ($type == 'login' && Context::get('is_logged'))
 		{
-			throw new Rhymix\Framework\Exception('already_logged');
+			throw new \Rhymix\Framework\Exception('already_logged');
 		}
 
-		
 		// 인증 메일 유효 시간
 		if (self::getConfig()->mail_auth_valid_hour)
 		{
-			$args = new stdClass;
+			$args = new \stdClass;
 			$args->list_count = 5;
 			$args->regdate_less = date('YmdHis', strtotime(sprintf('-%s hour', self::getConfig()->mail_auth_valid_hour)));
 			$output = executeQueryArray('sociallogin.getAuthMailLess', $args);
 
 			if ($output->toBool())
 			{
-				$oMemberController = getController('member');
+				$oMemberController = MemberController::getInstance();
 
 				foreach ($output->data as $key => $val)
 				{
@@ -118,7 +133,7 @@ class SocialloginView extends \Rhymix\Modules\Sociallogin\Base
 		$this->setRedirectUrl($oDriver->createAuthUrl($type));
 
 		// 로그 기록
-		$info = new stdClass;
+		$info = new \stdClass;
 		$info->sns = $service;
 		$info->type = $type;
 		SocialloginModel::logRecord($this->act, $info);
@@ -127,21 +142,21 @@ class SocialloginView extends \Rhymix\Modules\Sociallogin\Base
 	/**
 	 * @brief SNS 프로필
 	 */
-	function dispSocialloginSnsProfile()
+	public function dispSocialloginSnsProfile()
 	{
 		if (self::getConfig()->sns_profile != 'Y')
 		{
-			throw new Rhymix\Framework\Exceptions\InvalidRequest();
+			throw new InvalidRequest;
 		}
 
 		if (!Context::get('member_srl'))
 		{
-			throw new Rhymix\Framework\Exceptions\InvalidRequest();
+			throw new InvalidRequest;
 		}
 
-		if (!($member_info = memberModel::getMemberInfoByMemberSrl(Context::get('member_srl'))) || !$member_info->member_srl)
+		if (!($member_info = MemberModel::getMemberInfoByMemberSrl(Context::get('member_srl'))) || !$member_info->member_srl)
 		{
-			throw new Rhymix\Framework\Exceptions\InvalidRequest();
+			throw new InvalidRequest;
 		}
 
 		Context::set('member_info', $member_info);
@@ -153,7 +168,7 @@ class SocialloginView extends \Rhymix\Modules\Sociallogin\Base
 				continue;
 			}
 
-			$args = new stdClass;
+			$args = new \stdClass;
 			$args->profile_name = $sns_info->name;
 			$args->profile_url = $sns_info->profile_url;
 			$args->service = $val;
